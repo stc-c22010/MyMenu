64ceb591d3483d241fcce26859fd15c76b660aeb
stc-c22010 <c22010@suntech.jp>
2023-09-26 14:12:44 +0900

    activity_menu_thanks.xml menu_options_menu_list.xml MenuThanksActivity.java

--- app/src/main/java/jp/suntech/c22010/mymenu/MenuThanksActivity.java
+++ app/src/main/java/jp/suntech/c22010/mymenu/MenuThanksActivity.java
@@ -2,13 +2,30 @@ package jp.suntech.c22010.mymenu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MenuThanksActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_thanks);
+
+        Intent intent = getIntent();
+
+        String menuName = intent.getStringExtra("menuName");
+        String menuPrice = intent.getStringExtra("menuPrice");
+
+        TextView tvMenuName = findViewById(R.id.tvMenuName);
+        TextView tvMenuPrice = findViewById(R.id.tvMenuPrice);
+
+        tvMenuName.setText(menuName);
+        tvMenuPrice.setText(menuPrice);
+    }
+
+    public void onBackButtonClick(View view){
+        finish();
     }
 }
