8fa515161aab366aee57902651059effd5304628
stc-c22010 <c22010@suntech.jp>
2023-09-26 14:42:18 +0900

    activity_menu_thanks.xml MenuThanksActivity.java

--- app/src/main/java/jp/suntech/c22010/mymenu/MenuThanksActivity.java
+++ app/src/main/java/jp/suntech/c22010/mymenu/MenuThanksActivity.java
@@ -1,9 +1,11 @@
 package jp.suntech.c22010.mymenu;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -23,9 +25,27 @@ public class MenuThanksActivity extends AppCompatActivity {
 
         tvMenuName.setText(menuName);
         tvMenuPrice.setText(menuPrice);
+
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 
-    public void onBackButtonClick(View view){
-        finish();
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        boolean returnVal = true;
+
+        int itemId = item.getItemId();
+
+        if(itemId == android.R.id.home){
+            finish();
+        }
+        else{
+            //親クラスの同名メソッドを呼び出して、その戻り値をreturnValにする。
+            returnVal = super.onOptionsItemSelected(item);
+        }
+        return returnVal;
     }
 }
